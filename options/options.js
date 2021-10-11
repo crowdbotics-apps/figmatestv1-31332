edfbe7f32eadf1f83c17d18d0f9c72c416d6fe74
Crowdbotics <team@crowdbotics.com>
2021-10-11 19:47:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "figmatestv1_31332",
+  url: "https://figmatestv1_31332.botics.co",
+  api: "https://figmatestv1_31332.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
