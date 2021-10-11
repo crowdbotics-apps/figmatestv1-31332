d3667c2a8bc66b35776c34c514a2694bdea1ad21
Jesus Viloria <jesusviloria@Jesuss-MacBook-Air.local>
2021-10-11 17:29:02 -0400

    added the screens to the navigator

--- modules/yourapp.js
+++ modules/yourapp.js
@@ -1,5 +1,33 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
+import { Blank } from "../screens/Mostly1ColumnScreen(10414:181)6263987_10414_181/index"
+import { Blank as Blank2 } from "../screens/2ColumnTileList(10413:3)11263985_10413_3/index"
+import { Blank as Blank3 } from "../screens/DetailsScreen(10413:40)11263986_10413_40/index"
+import { Blank as Blank4 } from "../screens/ImageCardList(10417:1087)6263984_10417_1087/index"
+
+const FirstPage = () => {
+  return (
+    <Blank />
+  )
+};
+
+const SecondPage = () => {
+  return (
+    <Blank2 />
+  )
+};
+
+const ThirdPage = () => {
+  return (
+    <Blank3 />
+  )
+};
+
+const FourthPage = () => {
+  return (
+    <Blank4 />
+  )
+};
 
 const YourApp = () => {
   return (
@@ -25,7 +53,7 @@ export const YourAppModule = {
   path: "./",
   package: "@modules",
   value: {
-    title: "Your App",
-    navigator: YourApp
+    title: "My Ticket",
+    navigator: FirstPage
   }
 }
